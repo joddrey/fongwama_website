b6b51ae7bc8fdc493ec449b5db319ebf06f57a0f
joddrey <joddreyleza@gmail.com>
2021-04-28 13:37:20 +0100

    configuration du projet

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <h1>FONGWAMA WEBSITE</h1>
 </template>
 
 <style lang="scss">
--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import Accueuil from '../views/Accueuil.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Accueuil',
+    component: Accueuil
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '',
+    name: '',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    //component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
 ]
 
